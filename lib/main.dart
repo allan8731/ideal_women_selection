eb5babcebfaa60eace7b22bff912c000f012dcaf
allan8731 <104746443+allan8731@users.noreply.github.com>
2022-05-21 12:45:24 +0900

    Add init_setting

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+// 주제
+//
+// 소개팅을 회사에서 알란(이름)에게 어떤 이상형을 원하는지 묻습니다.
+//
+// 이상형란에는 머리 길이, 키, 나이를 넣으면 당신의 이상형 이름이 나옵니다.
+
+
+// 수도 코드 쓰는 방법
+// 커밋할수있는 부분으로 나눈다.
+// 일단 다 적어보자.
+// 1. class SelectIdeal todo::2 이름 변경 필요
+// 1.2 method
+// 1.2.4 headSizeChecker -> todo::3 allan 의 선택과 비교를 어떻게 할것인가?
+// 1.2.5 heightChecker ->
+// 1.2.6 ageChecker ->
+
+// 2. class IdealWomen todo::1 더미데이터의 완성
+// 2.1 field
+// 2.1.1 List<Map<String num>> womenList
+
+// 3. class SerializeWomenJsonData
+// 3.1 field
+// 3.1.1 name
+// 3.1.2 age
+// 3.1.3 height
+// 3.1.4 headSize
+// 3.2 method
+// 3.2.1 {"name":String "심주연" , "age":int 34 , "headSize": double 50 , "height": double 170}
+
+//4. class IdealChecker
+
+//5. class HeadSizeChecker
+//6. class HeightChecker
+//7. class AgeChecker
